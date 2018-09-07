08c0a2b5dc7a73a631a6ef28ae69d53849dd5055
Ashish Kurian Thomas <box.ashishk@gmail.com>
2018-09-07 23:05:24 +0530

    removed body parser for multer

---
+++ paste.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const PasteSchema = new mongoose.Schema(
+  {
+    id_gen: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    created_dt: {
+      type: Date,
+      default: new Date()
+    }
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('paste', PasteSchema);
--- server.js
+++ server.js
@@ -4,31 +4,49 @@ const morgan = require('morgan');
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const bp = require('body-parser');
+
 const random = require('crypto-random-string');
+const multer = require('multer');
+const upload = multer();
 
 const app = express();
-
-// request body access
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
 
 // enable logging
 app.use(morgan('short'));
 
 // connecting to our database
-mongoose.connect(
-  config.db,
-  { useMongoClient: true }
-);
+mongoose.connect(config.db);
 mongoose.connection.on('error', console.error.bind(console, '[spb] db connection error'));
 mongoose.connection.once('open', () => {
   console.log('[spb] db connection sucess');
 });
 
+const Paste = require('./paste.model');
+
 // routes - start
 app.get('/h', (req, res) => {
-  return res.status(200).send('OK');
+  return res.status(200).send('OK\n');
+});
+
+app.post('/', upload.none(), async (req, res) => {
+  if (!req.body.spb) {
+    return res.status(400).send('bad data\n');
+  }
+
+  let gid = random(6);
+
+  let n_paste = new Paste({
+    id_gen: gid,
+    content: req.body.spb
+  });
+
+  let n_save = await n_paste.save();
+
+  if (!n_save) {
+    return res.status(500).send('save failed\n');
+  }
+
+  return res.status(200).send(`${config.host}/${gid}\n`);
 });
 
 // routes - end
